9f4156eea1896859fa21cdb9033719bd90e707b5
tuoitrelacloi <luonglx@protonmail.com>
2018-10-03 18:19:26 -0400

    Add 2 file by Pycharm

--- Debian-9_TemplateVM.sh
+++ Debian-9_TemplateVM.sh
@@ -1,1 +1,27 @@
-#!
+#!/bin/sh
+#Run in Debian TemplateVM
+# ---------------------------------------------------------------------
+# Debian getting startup script.
+# ---------------------------------------------------------------------
+#0. update system
+sudo apt-get update && sudo apt-get upgrade
+
+# I don't know why debian was not pre-installed file manager, so I install nautilus
+sudo apt-get install -y nautilus
+
+#2. install Atom
+wget https://atom.io/download/deb -O atom.deb
+sudo dpkg -i atom.deb
+sudo apt --fix-broken install
+
+#3. install vlc
+sudo apt-get install -y vlc
+
+#4. install Python-Pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-tk
+sudo get-apt install -y python3-venv
+sudo apt-get install -y python3-yaml
+sudo apt-get install -y python3-dev
+sudo apt-get install -y python-yaml
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Qbes-Scrypt
-Some scripts use for qubes-os
+Some scripts use for qubes-os R4.0
+1. Afte install and update qubes in qube-manager, run each scrip in proper templateVM. 
